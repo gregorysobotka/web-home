5b34c847006781050bf966336f7b8b7f6ea0f496
Gregory Sobotka <gregorysobotka@gmail.com>
2020-10-18 13:58:29 -0700

    init project

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <!-- <div class="d-flex align-center">
+       
+      </div> -->
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        to="/settings"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Settings</span>
+        <v-icon>mdi-cog</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: "App",
+
+  components: {
+    
+  },
+
+  data: () => ({
+    //
+  })
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <Favorites />
+</template>
+
+<script>
+// @ is an alias to /src
+import Favorites from "@/components/Favorites.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Favorites
+  }
+};
+</script>
